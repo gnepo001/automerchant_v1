a59a9970170e13a16ed42bd627896637967c9439
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-11-10 23:11:56 -0800

    testing

---
+++ client/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="pt-3 flex flex-row justify-between items-center">
+        <span>test1</span>
+        <span>test2</span>
+        <span>test3</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Header } from "../components/index";
+
+const index = () => {
+  return (
+    <div>
+      <Header />
+      index
+    </div>
+  );
+};
+
+export default index;
