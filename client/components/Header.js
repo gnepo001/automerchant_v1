adac7123a690752b51dcf35b6ec3e49a219609d4
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-11-11 20:10:55 -0800

    css changes

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,13 @@
 const Header = () => {
   return (
     <div>
-      <div className="pt-4 flex flex-row justify-between items-center">
-        <span>test1</span>
-        <span>test2</span>
-        <span>test3</span>
+      <div className="pt-4 flex flex-row justify-between p-5">
+        <span className="font-bold text-green-600">Auto Merchant</span>
+        <span className="border-2 border-green-900 rounded-md bg-green-600 px-1 text-white">
+          Log In
+        </span>
       </div>
+      <div className="border-b-2 border-zinc-400 mx-4" />
     </div>
   );
 };
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,5 @@
+// REQUIRED For Tailwind CCS to work
+
 import "./globals.css";
 
 export default function App({ Component, pageProps }) {
