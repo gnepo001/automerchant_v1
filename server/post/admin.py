c8cec0442debb9dc227440ec0b5f67f4816db15c
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-11-19 22:59:01 -0800

    fixed issues

--- server/post/admin.py
+++ server/post/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+#this will display the model in admin site
+admin.site.register(Post)
--- server/server/settings.py
+++ server/server/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #### user created apps
     'post',
 ]
 
