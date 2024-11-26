2116e7ccf77d33ab1c2f4fec76d53ccb2dc696d5
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-11-25 22:59:49 -0800

    created custom users and admins

--- server/server/settings.py
+++ server/server/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'post',
+    'accounts.apps.AccountsConfig'
 ]
+
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
