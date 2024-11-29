b412c76f47586b3d8e021ec9000a8b0a220f931f
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-11-28 21:41:31 -0800

    added user to page

--- client/components/Header.js
+++ client/components/Header.js
@@ -4,7 +4,11 @@ const Header = () => {
       <div className="pt-4 flex flex-row justify-between p-5">
         <span className="font-bold text-green-600">Auto Merchant</span>
         <span className="border-2 border-green-900 rounded-md bg-green-600 px-1 text-white">
-          Log In
+          {user.is_authrnticated ? (
+            <span>Hello there</span>
+          ) : (
+            <span>Login In</span>
+          )}
         </span>
       </div>
       <div className="border-b-2 border-zinc-400 mx-4" />
